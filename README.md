71a85727a39f8c066972bfa8b86579b1f3fe53b2
fasha daba <ilhamnn17@gmail.com>
2024-12-22 19:58:36 +0700

    1

---
+++ README.md
@@ -0,0 +1,127 @@
+
+# Submission UAP ML VIIB
+## 🦖 EggSpector: Klasifikasi Citra Jenis Telur Ayam & Bebek Dengan Mengimplementasikan VGG-16 & MobileNetV2
+
+
+
+
+![Egs](assets/rdm/cv.jpg)
+
+
+## Deskripsi Project
+
+Proyek ini bertujuan untuk mengembangkan sistem klasifikasi gambar yang dapat mengenali dan membedakan jenis telur unggas, yaitu telur ayam dan bebek, menggunakan arsitektur deep learning **VGG-16** dan **MobileNetV2**. Dengan memanfaatkan dataset gambar telur, sistem ini dirancang untuk membantu peternak dan pelaku agribisnis dalam mengidentifikasi jenis telur secara otomatis, sehingga meningkatkan efisiensi proses sortir, pengelolaan hasil ternak, dan pengambilan keputusan yang lebih cepat serta akurat.
+
+Dataset yang digunakan dapat anda akses pada link : [Egg Classification](https://www.kaggle.com/datasets/gauravduttakiit/automated-egg-classification).
+
+Arsitektur yang digunakan adalah model pre-trained yaitu VGG-16 dan MobileNetV2 dengan model arsitektur seperti gambar dibawah ini.
+
+#### VGG-16
+![VGG-16](assets/rdm/vgg16.jpg)
+
+
+#### MobileNetV2
+![MobileNetV2](assets/rdm/V2.png)
+
+## Overview Dataset
+
+Dataset  klasifikasi jenis telur yang digunakan bersumber dari platform Kaggle dengan total 2 class yang total datanya berjumlah 14.7k file. Pembagian data split dengan rasio 80:10:10 tvt, pada dataset ini terdapat 2 Label Class, yaitu : 🐔 chicken & 🦆 duck.
+
+
+## Langkah Instalasi
+
+Clone the project
+```bash
+  git clone https://github.com/ilhamnn/UAP-ML-7B
+```
+
+Go to the project directory
+
+```bash
+  cd project folder
+```
+
+Start the server
+```bash
+  streamlit run ./view/src/lut.py
+```
+
+
+
+## Deskripsi Model
+
+#### Cleaning
+
+Beberapa gambar yang tidak sesuai akan dihapus untuk meningkatkan kualitas data yang digunakan dalam pelatihan.
+
+#### Pre-processing
+
+Pengaturan untuk Training dengan ImageDataGenerator
+
+Menggunakan ImageDataGenerator untuk memuat dan memproses gambar dari direktori yang ditentukan. Gambar akan diubah ukurannya menjadi 224x224 piksel dengan ukuran batch 8.
+
+- Melakukan rescaling untuk menormalkan nilai piksel ke rentang [0, 1].
+
+- Kualitas gambar juga dikurangi dengan menurunkan resolusi untuk mempercepat proses pelatihan dan mengurangi waktu komputasi.
+
+- Untuk data validasi dan pengujian, hanya dilakukan rescaling tanpa augmentasi agar data tetap konsisten dan menjaga integritasnya selama evaluasi model.
+
+Dengan konfigurasi ini, model dapat belajar dari variasi gambar yang ada selama pelatihan, sementara preprocessing untuk evaluasi tetap sesuai dan konsisten.
+
+### **VGG-16**
+
+![paramsvgg](/assets/rdm/vggparams.jpg)
+
+Model ini menggunakan VGG-16 dengan bobot pra-latih dari ImageNet sebagai extractor fitur. Layer tambahan terdiri dari Global Average Pooling, dense 256 unit (ReLU), Dropout 0.3, dan output 2 unit (Softmax).
+Dari total 14,846,530 parameter, 131,842 dapat dilatih, 
+konfigurasi ini memanfaatkan VGG-16 untuk ekstraksi fitur dan melatih layer tambahan untuk klasifikasi jenis telur.
+
+
+Hasil dari model :
+
+![grafikmn](assets/rdm/vgg.png)
+
+![confmnv2](assets/rdm/vg.png)
+
+![crmnv](assets/rdm/crvgg.jpg)
+
+
+### Analisis Hasil Evaluasi Model
+
+**Performa**:
+
+Model VGG menghasilkan metrik sempurna pada data uji dengan precision, recall, dan F1-score sebesar 1.00 untuk kedua kelas (Ayam dan Bebek). Hal ini menunjukkan bahwa VGG mampu mengklasifikasikan semua sampel secara akurat tanpa kesalahan.
+
+**Training**:
+
+Grafik menunjukkan konvergensi bertahap, dengan penurunan loss yang stabil pada training dan validation. Akurasi mencapai 1.00 pada validation setelah beberapa epoch, mencerminkan proses pembelajaran yang konsisten.
+Meski performanya baik, pola grafik mengindikasikan kemungkinan model overfitting terhadap data training karena loss sangat rendah tanpa perbedaan signifikan dengan validation loss.
+
+
+
+### **MobileNetV2**
+
+![paramsmnv2](/assets/rdm/mnv2params.jpg)
+
+Model ini menggunakan MobileNetV2 dengan bobot pra-latih dari ImageNet sebagai extractor fitur. Layer tambahan terdiri dari Global Average Pooling, dense 256 unit (ReLU), Dropout 0.3, dan output 4 unit (Softmax).
+Dari total 2,586,434 parameter, 1,214,530 dapat dilatih.
+
+
+Hasil dari model :
+
+![grafikmn](assets/rdm/mnv2.png)
+
+![confmnv2](assets/rdm/mn.png)
+
+![crmnv](assets/rdm/crmn.jpg)
+
+### Analisis Hasil Evaluasi Model
+**Performa**:
+
+MobileNetV2 menghasilkan metrik sempurna pada data uji dengan precision, recall, dan F1-score sebesar 1.00 untuk kedua kelas. Hal ini menunjukkan kemampuan klasifikasi yang setara dengan VGG dalam hal akurasi.
+
+**Training**:
+
+Grafik menunjukkan konvergensi yang sangat cepat, di mana training dan validation loss mendekati nol dalam beberapa epoch awal. Akurasi juga langsung mencapai 1.00 tanpa fluktuasi berarti.
+Hal ini mencerminkan efisiensi MobileNetV2 yang dirancang untuk kinerja optimal dengan arsitektur yang ringan.
+
---
+++ src/view/lut.py
@@ -0,0 +1,227 @@
+import streamlit as st
+import tensorflow as tf
+from tensorflow.keras.preprocessing import image as keras_image
+from pathlib import Path
+import numpy as np
+from PIL import Image
+import io
+import base64
+
+st.set_page_config(page_title="EggSpector", page_icon="🦖")
+st.title("EggSpector")
+
+
+# CSS
+def set_background(image_file):
+    with open(image_file, "rb") as f:
+        img_data = f.read()
+    b64_encoded = base64.b64encode(img_data).decode()
+    style = f"""
+        <style>
+        .stApp {{
+            background-image: url(data:image/png;base64,{b64_encoded});
+            background-size: cover;
+            background-repeat: no-repeat;
+            background-attachment: local;
+        }}
+        .overlay {{
+            position: fixed;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+            background-color: rgba(0, 0, 0, 0.5);
+            z-index: 1;
+        }}
+        .content {{
+            position: relative;
+            z-index: 2;
+            padding: 0px;
+            margin: 0px;
+        }}
+        [data-testid="stVerticalBlockBorderWrapper"] {{
+            background-color: rgba(0, 0, 0, 0.5) !important;
+            border: none !important;
+            padding: 15px !important;
+            width: 100% !important;
+            border-radius: 10px !important;
+            margin-bottom: 5px !important;
+            backdrop-filter: blur(5px) !important;
+        }}
+        [class="st-emotion-cache-10trblm e1nzilvr1"] {{
+            justify-content: center !important;
+            text-align: center !important;
+            width: 100% !important;
+        }}
+        </style>
+    """
+    st.markdown(style, unsafe_allow_html=True)
+
+
+# Set background
+set_background("./assets/images/bg.jpg")
+
+
+# Fungsi untuk preprocessing gambar
+def preprocess_image(image_bytes):
+    img = Image.open(io.BytesIO(image_bytes)).convert("RGB")
+    img = img.resize((224, 224), Image.LANCZOS)
+    img_array = keras_image.img_to_array(img)
+    img_array = img_array / 255.0
+    img_array = np.expand_dims(img_array, axis=0)
+    return img_array
+
+
+# Fungsi untuk prediksi gambar
+def predict_image(img_array, model_path):
+    class_names = ["Telur Ayam", "Telur Bebek"]
+
+    try:
+        model = tf.keras.models.load_model(model_path)
+        output = model.predict(img_array)
+        score = tf.nn.softmax(output[0])
+        confidence = np.max(score)
+
+        if confidence < 0.40:
+            return None, confidence
+
+        predicted_class = class_names[np.argmax(score)]
+        return predicted_class, confidence
+    except Exception as e:
+        st.error(f"Terjadi kesalahan saat pemrosesan gambar/prediksi: {e}")
+        return None, None
+
+
+def resize_image(image_path, width, height):
+    img = Image.open(image_path)
+    img = img.resize((width, height))
+    return img
+
+
+# NAVIGATION in Sidebar
+st.sidebar.title("Navigation")
+
+# Radio untuk navigasi
+navigation = st.sidebar.radio("", ["Spector", "About", "Type"])  # Menghapus teks label
+
+
+if navigation == "Spector":
+    st.header("Ayo Cari Tahu Jenis Telurmu 🗿")
+    option = st.selectbox(
+        label="Pilih Model",
+        options=("VGG16", "MobileNetV2"),
+        index=None,
+        placeholder="Pilih Metode Yang Akan Digunakan...",
+    )
+
+    col1, col2 = st.columns([3, 2])
+
+    with col1:
+        upload = st.file_uploader(
+            "Unggah gambar telur untuk mendapatkan hasil prediksi",
+            type=["jpg", "jpeg", "png"],
+        )
+    with col2:
+        st.subheader("Hasil prediksi:")
+    with col1:
+        if st.button("Predict", type="primary"):
+            if upload is not None:
+                try:
+                    with st.spinner("Memproses gambar untuk prediksi..."):
+                        if option == "VGG16":
+                            model_path = Path("./src/model/vgg16.keras")
+                        elif option == "MobileNetV2":
+                            model_path = Path("./src/model/mnv2.keras")
+                        else:
+                            st.error("Model tidak valid!")
+                            st.stop()
+
+                        image_bytes = upload.getvalue()
+                        img_array = preprocess_image(image_bytes)
+                        result, confidence = predict_image(img_array, model_path)
+
+                    with col2:
+                        img = Image.open(upload)
+                        img = img.resize((300, 200))
+                        st.image(img, caption="Gambar yang diunggah")
+                        if result is None:
+                            st.error(
+                                "⚠️ Gambar yang diunggah tidak termasuk dalam kategori yang dikenali!"
+                            )
+                            if confidence is not None:
+                                with col1:
+                                    st.warning(
+                                        f"""
+                                        Tingkat keyakinan model: {confidence:.2%}
+
+                                        Model saat ini hanya dapat mendeteksi:
+                                        - Telur Ayam
+                                        - Telur Bebek
+                                    """
+                                    )
+                        else:
+                            st.success(f"Hasil Prediksi: **{result}**")
+                            st.write(f"Tingkat Kepercayaan: **{confidence:.2%}**")
+
+                except Exception as e:
+                    st.error(f"Terjadi kesalahan saat memproses gambar: {e}")
+            else:
+                st.warning("Unggah gambar terlebih dahulu!!")
+
+elif navigation == "About":
+    st.header("Hey, Selamat Datang di EggSpector! 🕵️🥚")
+    st.write(
+        """
+        **EggSpector** hadir untuk membantu kamu, peternak dan pengusaha telur, 
+        membedakan telur **ayam** dan **bebek** dengan cepat dan mudah! 🚀  
+        Kita tahu, kadang susah bedain telur cuma dari cangkangnya, tapi tenang, 
+        EggSpector punya **mata AI super tajam** buat bantuin kamu! 👀✨  
+
+        **Apa aja yang bisa EggSpector lakukan?**  
+        - Deteksi otomatis:  
+        🐓 **Telur Ayam**  
+        🦆 **Telur Bebek**  
+        - Super gampang: tinggal upload foto telur, dan BOOM—jawabannya langsung ada!  
+        - Akurasi tinggi karena kita pakai teknologi deep learning terkini. 💡  
+
+        Cobain sekarang dan lihat betapa kerennya si EggSpector ini!  
+        """
+    )
+
+elif navigation == "Type":
+    col1, col2 = st.columns(2)
+
+    width = 300  # Lebar yang diinginkan
+    height = 200  # Tinggi yang diinginkan
+
+    with col1:
+        img = resize_image("./assets/images/telurayam.jpg", width, height)
+        st.image(img, caption="Telur Ayam")
+        st.markdown(
+            """
+        **Penjelasan:**  
+        Telur ayam adalah jenis telur yang sering digunakan untuk kebutuhan sehari-hari dan merupakan sumber protein yang populer.  
+
+        **Ciri-ciri:**  
+        - Ukuran biasanya lebih kecil dibandingkan telur bebek.  
+        - Cangkang berwarna putih atau cokelat muda dengan tekstur halus.  
+        - Isi telur memiliki rasa yang ringan dan cocok untuk berbagai jenis masakan.  
+        - Lebih mudah ditemukan di pasaran dibandingkan telur bebek.
+        """
+        )
+
+    with col2:
+        img = resize_image("./assets/images/telurbebek.jpg", width, height)
+        st.image(img, caption="Telur Bebek")
+        st.markdown(
+            """
+        **Penjelasan:**  
+        Telur bebek biasanya digunakan untuk hidangan khas atau dibuat menjadi telur asin karena rasanya yang lebih kuat.  
+
+        **Ciri-ciri:**  
+        - Ukuran lebih besar dibandingkan telur ayam.  
+        - Cangkang berwarna putih atau abu-abu dengan tekstur kasar.  
+        - Isi telur lebih berminyak dengan rasa yang lebih kaya.  
+        - Biasanya digunakan untuk masakan khas seperti martabak atau telur asin.
+        """
+        )
